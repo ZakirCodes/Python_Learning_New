34237aae5d77c1046360e96fedee7e7cfdf178f9
Zakir Hussain <Zakirbhangwar.2007@gmail.com>
2026-07-22 10:51:05 +0500

    Update First_Program.py

--- Chap_01/First_Program.py
+++ Chap_01/First_Program.py
@@ -10,3 +10,14 @@ print(a)
 learner = 'Zakir'
 print("I am", learner, sep='-')
 
+# Using Python as Calculator
+print("4+5")
+print(3+4)
+
+# Variable
+b = 10
+c = 30
+print(b+c)
+print(b*c)
+print(c-b)
+print(c/b)
