251ba6ba5c093b645efd660cf4dc604ba50f3614
Zakir Hussain <Zakirbhangwar.2007@gmail.com>
2026-07-23 11:23:48 +0500

    Create Project_2.py

---
+++ Chap_02_Variable/Projects/Project_2.py
@@ -0,0 +1,15 @@
+# Gaming Profile
+
+username = "Username :"
+level = "Level    :"
+coin = "Coins    :"
+health = "Health   :"
+weapon = "Weapon   :"
+
+print("========== PLAYER ==========")
+print(username)
+print(level)
+print(coin)
+print(health)
+print(weapon)
+print("============================")
